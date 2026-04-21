0ead9db0621cefa44924dc26e56ce6d5fe00280e
rielphmnl <riel.phmnl@gmail.com>
2026-04-21 19:12:17 +0800

    fixed css of todo list

--- partials/head.php
+++ partials/head.php
@@ -11,12 +11,12 @@
 		<script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
 		<link rel="stylesheet" src="style.css">
 	</head>
-	<body class="h-screen">
+	<body class="h-screen w-screen">
 		<div class="absolute left-0 right-0 top-0 bottom-0 -z-10">
 			<img
 				src="https://plus.unsplash.com/premium_photo-1683309568772-57011d6c1b7b?fm=jpg&q=60&w=3000&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8dG8lMjBkbyUyMGxpc3R8ZW58MHx8MHx8fDA%3D"
 				alt="background"
-				class="h-full object-cover object-top-left"
+				class="h-full w-full object-cover object-top-left"
 			/>
 
 		</div>
--- views/index.view.php
+++ views/index.view.php
@@ -1,32 +1,35 @@
 <?php require $_SERVER['DOCUMENT_ROOT'] . '/partials/head.php'; ?>
 
-
-<div class="app">
-	<p>TODO LIST</p>
-	<div class="container">
-		<div class="list">
+<!-- app -->
+<div class="h-screen w-screen flex flex-col justify-center items-center">
+	<p class="text-2xl font-semibold">TODO LIST</p>
+	<!-- container -->
+	<div class="bg-neutral-300/60 rounded-xl p-5 mt-5 flex flex-col gap-2 items-center">
+		<!-- list -->
+		<div class="bg-neutral-300/80 rounded-xl p-5 flex flex-col gap-2">
 
 			<?php foreach ($todos as $todo) : ?>
-				<div class="item">
+				<div class="border border-neutral-400 rounded-lg flex gap-5 justify-between items-center py-3 px-5">
 					<p>
 						<?php echo $todo['isDone'] === 1 ? "<s>{$todo['task']}</s>" : "{$todo['task']}"; ?>
 					</p>
-					<div style="display:flex; align-items: center;">				
-						<a style="height:100%" class="button" href="/task.php?id=<?= $todo['id'] ?>"><p>view</p></a>
+					<div class="flex">
+						<a class="bg-neutral-300/95 border hover:bg-neutral-200 hover:scale-105 border-neutral-400 rounded-lg px-3 py-1 cursor-pointer" href="/task.php?id=<?= $todo['id'] ?>"><p>view</p></a>
 					</div>
 				</div>
 			<?php endforeach ?>
 
 
 		</div>
-		<div class="create-new">
-			<a class="button" href="/new.php">Create TODO</a>
+		<!-- create new -->
+		<div class="flex">
+			<a class="bg-neutral-300/95 border hover:bg-neutral-200 hover:scale-105 border-neutral-400 rounded-lg px-3 py-1 cursor-pointer" href="/new.php">Create TODO</a>
 		</div>	
 	</div>
 	
 	<!-- make logout button -->
-	<form action="/actions/logoutUser.php" method="POST">
-		<button>log out</button>
+	<form class="pt-2" action="/actions/logoutUser.php" method="POST">
+		<button class="bg-neutral-300/95 border hover:bg-neutral-200 hover:scale-105 border-neutral-400 rounded-lg px-3 py-1 cursor-pointer">log out</button>
 	</form>
 </div>
 	
